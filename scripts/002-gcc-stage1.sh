5cd758cf352f0a9198c63868f587d26ea7a4c15d
Wally4000 <catapultit@me.com>
2024-04-26 14:08:41 +1000

    Modify GCC Paths for sysroot?

--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -4,6 +4,7 @@
 if [[ $(uname) == "Darwin" ]]; then
 export CPATH="$(brew --prefix)/include" # This may break Ubuntu
 export LIBRARY_PATH := $(brew --prefix)/lib #This may break Ubuntu
+TARG_XTRA_OPTS="--with-sysroot --with-native-system-header=$(brew --prefix)/include"
 fi
 
 
